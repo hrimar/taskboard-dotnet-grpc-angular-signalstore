6efdc0cd382e0761a123a5a11b8c41ddffc915a6
Hristo <hhristoff@yahoo.com>
2026-09-22 13:11:43 +0300

    Added taskitem.proto, TaskItemGrpcService skeleton, registered in Program.cs

--- src/TaskBoard.Api/Program.cs
+++ src/TaskBoard.Api/Program.cs
@@ -11,6 +11,7 @@ var app = builder.Build();
 app.MapGrpcService<GreeterService>();
 app.MapGrpcService<LabelGrpcService>();
 app.MapGrpcService<BoardGrpcService>();
+app.MapGrpcService<TaskItemGrpcService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
---
+++ src/TaskBoard.Api/Services/TaskItemGrpcService.cs
@@ -0,0 +1,34 @@
+﻿using Grpc.Core;
+using Google.Protobuf.WellKnownTypes;
+using TaskBoard.Contracts.V1;
+
+namespace TaskBoard.Api.Services;
+
+public class TaskItemGrpcService : TaskItemService.TaskItemServiceBase
+{
+    private readonly ILogger<TaskItemGrpcService> _logger;
+    public TaskItemGrpcService(ILogger<TaskItemGrpcService> logger)
+    {
+        _logger = logger;
+    }
+    public override Task<TaskItem> CreateTaskItem(CreateTaskItemRequest request, ServerCallContext context)
+    {
+        throw new RpcException(new Status(StatusCode.Unimplemented, "Will be implemented with EF Core."));
+    }
+    public override Task<TaskItem> GetTaskItem(GetTaskItemRequest request, ServerCallContext context)
+    {
+        throw new RpcException(new Status(StatusCode.Unimplemented, "Will be implemented with EF Core."));
+    }
+    public override Task<ListTaskItemsResponse> ListTaskItems(ListTaskItemsRequest request, ServerCallContext context)
+    {
+        throw new RpcException(new Status(StatusCode.Unimplemented, "Will be implemented with EF Core."));
+    }
+    public override Task<TaskItem> UpdateTaskItem(UpdateTaskItemRequest request, ServerCallContext context)
+    {
+        throw new RpcException(new Status(StatusCode.Unimplemented, "Will be implemented with EF Core."));
+    }
+    public override Task<Empty> DeleteTaskItem(DeleteTaskItemRequest request, ServerCallContext context)
+    {
+        throw new RpcException(new Status(StatusCode.Unimplemented, "Will be implemented with EF Core."));
+    }
+}
