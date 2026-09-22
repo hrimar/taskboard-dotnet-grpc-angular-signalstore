cb21f3a9ad776d66f404d895ff0839177ceefca0
Hristo <hhristoff@yahoo.com>
2026-09-22 11:32:44 +0300

    Added board.proto, BoardGrpcService skeleton & service registration in Program

---
+++ src/TaskBoard.Api/Services/BoardGrpcService.cs
@@ -0,0 +1,40 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using TaskBoard.Contracts.V1;
+
+namespace TaskBoard.Api.Services;
+
+public class BoardGrpcService : BoardService.BoardServiceBase
+{
+    private readonly ILogger<BoardGrpcService> _logger;
+
+    public BoardGrpcService(ILogger<BoardGrpcService> logger)
+    {
+        _logger = logger;
+    }
+
+    public override Task<Board> CreateBoard(CreateBoardRequest request, ServerCallContext context)
+    {
+        throw new RpcException(new Status(StatusCode.Unimplemented, "Will be implemented with EF Core."));
+    }
+
+    public override Task<Board> GetBoard(GetBoardRequest request, ServerCallContext context)
+    {
+        throw new RpcException(new Status(StatusCode.Unimplemented, "Will be implemented with EF Core."));
+    }
+
+    public override Task<ListBoardsResponse> ListBoards(ListBoardsRequest request, ServerCallContext context)
+    {
+        throw new RpcException(new Status(StatusCode.Unimplemented, "Will be implemented with EF Core."));
+    }
+
+    public override Task<Board> UpdateBoard(UpdateBoardRequest request, ServerCallContext context)
+    {
+        throw new RpcException(new Status(StatusCode.Unimplemented, "Will be implemented with EF Core."));
+    }
+
+    public override Task<Empty> DeleteBoard(DeleteBoardRequest request, ServerCallContext context)
+    {
+        throw new RpcException(new Status(StatusCode.Unimplemented, "Will be implemented with EF Core."));
+    }
+}
